6487b16d952706944674885a64831db8b62ce875
Mantasha Jamadar <mantashajamadar55@gmail.com>
2026-09-21 14:06:54 +0530

    Update spam detection model and frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -219,7 +219,7 @@ function App() {
     setResult(null)
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+     const response = await fetch('https://spamshield-backend-byjx.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
